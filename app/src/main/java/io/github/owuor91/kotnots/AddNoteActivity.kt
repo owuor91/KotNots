9fe3eb58f0d4e0a3389037e758331dc23978e7db
John Owuor <owuor91@gmail.com>
2019-08-13 00:36:00 +0300

    first commit

---
+++ app/src/main/java/io/github/owuor91/kotnots/AddNoteActivity.kt
@@ -0,0 +1,12 @@
+package io.github.owuor91.kotnots
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddNoteActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_add_note)
+  }
+}
---
+++ app/src/main/java/io/github/owuor91/kotnots/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.github.owuor91.kotnots
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
